47b4f5a8cf07af0186700d46c4b12d5be6c3cb7f
Adam Laughlin <adam.laughlin@gmail.com>
2019-03-03 09:53:30 -0700

    fixed demo

--- src/mobile/App.js
+++ src/mobile/App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
