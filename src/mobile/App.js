de3c08dd076d075499d1e946f90016689a1b2f1b
Adam Laughlin <adam.laughlin@gmail.com>
2019-03-05 20:10:35 -0700

    moved sample directory to mobile assets to get data rendering

--- src/mobile/App.js
+++ src/mobile/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import data from './assets/sample_data/ml_output_foo.json';
+import {Svg} from 'expo';
+const {Rect, Polyline } = Svg;
 
 const styles = StyleSheet.create({
   container: {
@@ -10,13 +13,21 @@ const styles = StyleSheet.create({
   },
 });
 
-
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Svg height="90%" width="100%" >
+          <Polyline
+              points={data.reduce((prev,y,i)=>prev+`${i*10},${y} `,"")}
+              fill="none"
+              stroke="black"
+              strokeWidth="3"
+          />
+        </Svg>
       </View>
+
     );
   }
 }
