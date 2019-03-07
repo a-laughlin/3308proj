a417a54c8bcdc89a96387d0eef80da708e4dc64e
Adam Laughlin <adam.laughlin@gmail.com>
2019-03-06 17:05:38 -0700

    mobile svg working + setup script

---
+++ setup.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+if [[ $SHELL != *bash ]]; then
+  echo Bash shell required for installation.
+elif ! [[ -f "./setup.sh" ]]; then
+  echo Must be in project root directory before running script
+else
+  configfile="~/.bashrc"
+  [[ $OSTYPE = *darwin* ]] && configfile="~/.bash_profile"
+
+  if ! [[ $(command -v brew) ]]; then
+    if [[ $OSTYPE != *darwin* ]]; then #on linux
+      sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    else # on mac
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+  fi
+
+  # bash scripts can't run another script without permissions, so check if the file exists
+  if ! [[ -f $NVM_DIR/nvm.sh ]]; then
+    brew install nvm
+  fi
+  # set up nvm if it isn't in the path
+  if [[ $PATH != *$HOME/.nvm/* ]]; then
+    echo 'export NVM_DIR="$HOME/.nvm"' >> $configfile
+    echo '[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm'  >> $configfile
+    echo '[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion'  >> $configfile
+    source $configfile
+  fi
+
+  if ! [[ $(command -v node) ]]; then
+    nvm install node
+    nvm use node
+  fi
+
+  if ! [[ $(command -v yarn) ]]; then
+    brew install yarn --ignore-dependencies
+    yarn config set prefix ~/.yarn
+  fi
+
+  if ! [[ $(command -v expo) ]]; then
+    yarn global add expo-cli
+  fi
+
+  if ! [[ $(command -v git) ]]; then
+    brew install git
+  fi
+
+
+  ginfo=$(git status -sb | head -n1) # outputs ## <branchname>...origin/master [ahead 3]
+
+  # ensure git always pulls from origin master on this branch
+  [[ $ginfo != *origin/master* ]] && git branch --set-upstream-to origin/master
+
+  # ensure latest mobile dependencies installed
+  cd src/mobile
+  yarn install
+  cd -
+
+  # check branch behind
+  [[ $ginfo = *behind* ]] && echo "WARNING: git pull origin master needed, then re-run this script"
+
+  if [[ $(python3 --version) != *"3.7.2"* ]]; then
+    echo "PLEASE INSTALL PYTHON 3.7.2"
+    echo "if you're unfamiliar with installing different versions, try:"
+    echo "brew install pyenv"
+    echo "pyenv install 3.7.2"
+    echo "pyenv local 3.7.2"
+  fi
+
+fi
--- src/mobile/App.js
+++ src/mobile/App.js
@@ -13,19 +13,20 @@ const styles = StyleSheet.create({
   },
 });
 
-// <Svg height="90%" width="100%" >
-//   <Polyline
-//       points={data.reduce((prev,y,i)=>prev+`${i*10},${y} `,"")}
-//       fill="none"
-//       stroke="black"
-//       strokeWidth="3"
-//   />
-// </Svg>
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Svg height="90%" width="100%" >
+          <Polyline
+              points={data.reduce((prev,y,i)=>prev+`${i*10},${y} `,"")}
+              fill="none"
+              stroke="black"
+              strokeWidth="3"
+          />
+        </Svg>
       </View>
     );
   }
