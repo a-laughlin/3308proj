03521623e1b3d4a616b223d3fdd169c72181f1e0
Adam Laughlin <adam.laughlin@gmail.com>
2019-03-11 20:21:22 -0600

    remove debug code to get tests working locally

--- src/ml/RNN.py
+++ src/ml/RNN.py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import random
 
 import torch
@@ -16,7 +14,7 @@ class RNN(nn.Module):
 
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
         self.i2o = nn.Linear(input_size + hidden_size, output_size)
-        
+
         self.trained = False
 
     def forward(self, input, hidden):
@@ -73,50 +71,27 @@ class RNN(nn.Module):
                 loss.backward()
                 optimizer.step()
 
-                if epoch % 25 == 0:
-                    print('epoch {}, loss {}, teacher forcing: {}'.format(epoch, loss, use_teacher_forcing))
-                 
-                    
+
             self.trained = True
-            
+
         except KeyboardInterrupt:
             pass
-        
-        
+
+
     def predict(self, y, future = 50, plot = True):
-    
+
         hidden = self.initHidden()
-        
+
         input_y = y[:-1]
-        target_y = y[1:]
-        
+
         outputs = []
         for input in input_y.view(-1,1,1):
             output, hidden = self(input, hidden)
             outputs.append(output)
-        
+
         future_outputs = [output]
         for _ in range(future):
             output, hidden = self(output, hidden)
             future_outputs.append(output)
-            
-        #all_outputs = outputs + future_outputs
-        
-        pred_target_y = torch.Tensor(outputs).view(-1, 1)
-        future_target_y = torch.Tensor(future_outputs).view(-1, 1)
-        
-        #all_target_y = torch.Tensor(all_outputs).view(-1, 1)
-        
-        if plot:
-            with torch.no_grad():
-                t1 = np.arange(len(input_y))
-                t2 = np.arange(len(input_y) - 1, len(input_y) - 1 + len(future_outputs))
-                
-                plt.plot(t1, target_y.detach().numpy(), label = 'Past True', color = 'blue')
-                plt.plot(t1, pred_target_y.detach().numpy(), label = 'Past Prediction', color = 'red')
-                
-                plt.plot(t2, future_target_y.detach().numpy(), linestyle = '--', color = 'red', label = 'Future Prediction')
-                
-                plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0)
-                
-        return future_target_y
+
+        return torch.Tensor(future_outputs).view(-1, 1)
