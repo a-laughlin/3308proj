b7584c9b7d4b962db507237cf4fb9535d6d6cf72
Adam Laughlin <adam.laughlin@gmail.com>
2019-03-12 18:07:18 -0600

    update setup to install pipenv

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 
 if [[ $SHELL != *bash ]]; then
   echo Bash shell required for installation.
+elif [[ $PIPENV_ACTIVE = 1 ]]; then
+  echo Script not meant to be run in a pipenv virtual environment
 elif ! [[ -f "./setup.sh" ]]; then
   echo Must be in project root directory before running script
 else
-  configfile="~/.bashrc"
-  [[ $OSTYPE = *darwin* ]] && configfile="~/.bash_profile"
+  configfile="$HOME/.bashrc"
+  cfg="$(cat $configfile)"
+  ensure_active(){ cmd="$1"; eval "$cmd"; echo "$cmd" >> $configfile && echo "$cmd" ;}
 
   if ! [[ $(command -v brew) ]]; then
     if [[ $OSTYPE != *darwin* ]]; then #on linux
@@ -16,56 +19,47 @@ else
     fi
   fi
 
-  if ! [[ $(command -v git) ]]; then
-    brew install git
-  fi
+  # git
+  ! [[ $(command -v git) ]] && brew install git
 
+  # git configuration for push/pull
   ginfo=$(git status -sb | head -n1) # outputs ## <branchname>...origin/master [ahead 3]
+  [[ $ginfo != *origin/master* ]] && git branch --set-upstream-to origin/master # ensure git always pulls from origin master on this branch
+  [[ $ginfo = *behind* ]] && echo "WARNING: git pull needed, then re-run this script" && exit 1
 
-  # ensure git always pulls from origin master on this branch
-  [[ $ginfo != *origin/master* ]] && git branch --set-upstream-to origin/master
+  # nvm
+  ! [[ $(brew list nvm) ]] && brew install nvm
+  [[ "$cfg" != *'export NVM_DIR='* ]] && ensure_active "export NVM_DIR='$HOME/.nvm'"
+  [[ "$cfg" != *'/nvm/nvm.sh'* ]] && ensure_active "[ -s '/usr/local/opt/nvm/nvm.sh' ] && . '/usr/local/opt/nvm/nvm.sh';  # This loads nvm"
+  [[ "$cfg" != *'/nvm/etc/bash_completion'* ]] && ensure_active "[ -s '/usr/local/opt/nvm/etc/bash_completion' ] && . '/usr/local/opt/nvm/etc/bash_completion';  # This loads nvm bash_completion"
+  [[ "$cfg" != *'nvm install node'* ]] && ensure_active '! [[ $(command -v node) ]] && nvm install node '
+  [[ "$cfg" != *'export PATH="$(nvm which current)'* ]] && ensure_active '[[ "$PATH" != *"/node/"* ]] && export PATH="$(nvm which current):$PATH"'
 
-  # ensure branch up to date
-  [[ $ginfo = *behind* ]] && echo "WARNING: git pull origin master needed, then re-run this script" && exit 1
+  # yarn
+  ! [[ $(command -v yarn) ]] && brew install yarn --ignore-dependencies
 
-  # set up nvm if it isn't in the path
-  if [[ $PATH != *$HOME/.nvm/* ]]; then
-    # bash scripts can't run another script without permissions, so check if the file exists
-    if ! [[ -f $NVM_DIR/nvm.sh ]]; then
-      brew install nvm
-    fi
+  # create-react-app
+  ! [[ $(command -v create-react-app) ]] && yarn global add create-react-app
 
-    echo 'export NVM_DIR="$HOME/.nvm"' >> $configfile
-    echo '[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm'  >> $configfile
-    echo '[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion'  >> $configfile
-    source $configfile
+  # pyenv
+  ! [[ $(command -v pyenv) ]] &&  brew install pyenv
 
-    if ! [[ $(command -v node) ]]; then
-      nvm install node
-      nvm use node
-    fi
-  fi
+  # set global pyenv default
+  ! [[ $(pyenv global) ]] && pyenv install 3.7.2 && pyenv global 3.7.2
+  [[ "$cfg" != *'PYENV_ROOT'* ]] && ensure_active "export PYENV_ROOT='$(pyenv root)'"
 
-  if ! [[ $(command -v yarn) ]]; then
-    brew install yarn --ignore-dependencies
-    yarn config set prefix ~/.yarn
-  fi
-
-  if ! [[ $(command -v create-react-app) ]]; then
-    yarn global add create-react-app
-  fi
+  # pipenv
+  ! [[ $(brew list pipenv) ]] && brew install pipenv
+  [[ "$cfg" != *'PIPENV_CACHE_DIR'* ]] && ensure_active "export PIPENV_CACHE_DIR='$HOME/.pipenv/.packages/'"
+  [[ "$cfg" != *'WORKON_HOME'* ]] && ensure_active "export WORKON_HOME='$HOME/.pipenv/.venvs/'"
+  [[ "$cfg" != *'export PATH="$PYENV_ROOT:'* ]] && ensure_active "[[ \"\$PATH\" != *\"\$PYENV_ROOT\"* ]] && export PATH=\"\$PYENV_ROOT:\$PATH\""
 
   # ensure latest web dependencies installed
-  cd src/web
-  yarn install
-  cd -
+  builtin cd src/web
+  yarn install --frozen-lockfile
+  builtin cd -
 
-  if ! [[ $(command -v pipenv) ]]; then
-    echo "WARNING: Pipenv not installed. Please install pyenv and pipenv to run the pipenv virtual environment"
-  else
-    if ! [[ $PIPENV_ACTIVE ]]; then
-      pipenv install --skip-lock
-      pipenv shell
-    fi
-  fi
+  # ensure latest py dependencies installed
+  pipenv install --dev --skip-lock
+  pipenv shell
 fi
