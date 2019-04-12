9178b44b9c596402669dcca67e9f47306a9421e2
Adam Laughlin <adam.laughlin@gmail.com>
2019-04-12 17:12:34 -0600

    decouple prediction num i/o from file system

--- src/ml/predict.py
+++ src/ml/predict.py
@@ -1,47 +1,40 @@
 import argparse
 import json
-
+import sys
 import ml_utils
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description = "Predict future heartbeats given a previous window of data, and future timesteps to predict")
-    parser.add_argument('--input', type=str,
-                        default = '../web/src/sample_data/ml_input_foo.json',
-                        help = "Input file name containing window of data")
 
     parser.add_argument('--future', type=int,
                         default = 50,
                         help = "Future timesteps to predict")
 
+    parser.add_argument('--input', type=str,
+                        default = '[1,2,3,4,5,6]',
+                        help = "Input file name containing window of data")
+
     parser.add_argument('--model', type=str,
                         default = '../web/src/sample_data/ml_model_foo.pt',
                         help = 'Location of trained model')
 
-    parser.add_argument('--output', type=str,
-                        default = '../web/src/sample_data/ml_output_foo.json',
-                        help = 'Output filename containing prediction')
-
     args = parser.parse_args()
 
     input_model_file = args.model
-    input_array_file = args.input
-
-    output_file = args.output
+    assert(ml_utils.goodModelFileName(input_model_file) == 1)
+    model = ml_utils.loadModel(input_model_file)
 
     future_timesteps = args.future
 
-    assert(ml_utils.goodModelFileName(input_model_file) == 1)
-    assert(ml_utils.goodInputFileName(input_array_file) == 1)
+    input_list = ml_utils.parse_input_list(json.loads(args.input))
 
-    input_array = ml_utils.parse_input_file(input_array_file)
+    prediction = ml_utils.predict(model, input_list, future_timesteps)
 
-    model = ml_utils.loadModel(input_model_file)
-
-    prediction = ml_utils.predict(model, input_array, future_timesteps)
-
-    output = prediction.numpy().flatten().tolist()
-
-    with open(output_file, 'w') as f:
-        json.dump(output, f)
+    output = json.dumps(prediction.numpy().flatten().tolist())
+    sys.stdout.write(output)
+    sys.stdout.flush()
+    sys.exit(0)
+    # with open(output_file, 'w') as f:
+    #     json.dump(output, f)
