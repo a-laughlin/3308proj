0939c02ac135f8a11dbb85baf0509f22082da99c
Adam Laughlin <adam.laughlin@gmail.com>
2019-04-22 12:50:47 -0600

    enable graphiql in production - attempt 2

--- src/api/graphql-server.js
+++ src/api/graphql-server.js
@@ -1,9 +1,14 @@
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer,gql } = require('apollo-server-express');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs, // define what api can respond with
+  resolvers, // get data for responses
+  introspection:true, // enable querying __schema to get the schema as json
+  playground:true // enable graphiql in production
+});
 module.exports = server;
