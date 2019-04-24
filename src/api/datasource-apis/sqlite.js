d6daecf8bacb33c524944470de51111508ad7237
Adam Laughlin <adam.laughlin@gmail.com>
2019-04-24 16:08:08 -0600

    removed unnecessary parts
    file isn't used or imported yet, so no danger of committing directly to master

--- src/api/datasource-apis/sqlite.js
+++ src/api/datasource-apis/sqlite.js
@@ -1,8 +1,6 @@
 const SRC = `${__dirname}/../../`;
 const API = `${SRC}/api`;
 const DB = `${API}/db.sqlite`; // db file
-const SAMPLE_DATA = `${API}/sample_data`;
-const SLEEPS = `${SAMPLE_DATA}/sleeps.json`;
 
 // note: all datasource-apis functions return a promise
 // a common way to read data async data
@@ -10,7 +8,7 @@ const SLEEPS = `${SAMPLE_DATA}/sleeps.json`;
 const sqlite={};
 
 sqlite.createSleeps = (sleeps)=>{
-  // inserts sleeps objects into the database
+  // inserts a sleeps objects into the database
 }
 
 sqlite.readSleeps = ()=>{
@@ -21,6 +19,6 @@ sqlite.readSleeps = ()=>{
 module.exports = sqlite
 
 // need to run this once to get the current file into the db
-// require(`${__dirname}/filesystem.js`)
+// require(`./filesystem.js`)
 // .readSleeps()
 // .then(sqlite.createSleeps)
